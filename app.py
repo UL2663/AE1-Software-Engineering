a1b2cef056ff51e906b225282a271aaa580d5e30
UL2663 <115477884+UL2663@users.noreply.github.com>
2026-01-04 18:26:17 +0000

    Complete Manufacture API call
    Written manufacture API call to be tested

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ app = Flask(__name__)
 api_key = os.getenv("NEWS_API_KEY") ##pulls from server settings 
 last_day = (datetime.bstnow() - timedelta(hours=24).strftime("%Y-%m-%dT%H:%M:%SZ"))
 
-@app.route("/api/news")
+@app.route("/api/AInews")
 def news():
     url = "https://gnews.io/api/v4/search"
     params = {
-        "q":"artificial intelligce",
+        "q":"artificial intelligence",
         "lang" : "en",
         "from": last_day,
         "max": 5000,
@@ -22,3 +22,15 @@ def news():
     return jsonify(response.json()) ##returns result to browser
 
 
+@app.route("/api/ManAIPharmanews")
+def news():
+    url = "https://gnews.io/api/v4/search"
+    params = {
+        "q":"(artificial intelligence) AND Manufacturing OR Pharmaceuticals", # OR has higher precedence so should work, ref docs 
+        "lang" : "en",
+        "from": last_day,
+        "max": 5000,
+        "token": api_key
+    }
+    response = requests.get(url, params=params)
+    return jsonify(response.json()) ##returns result to browser
