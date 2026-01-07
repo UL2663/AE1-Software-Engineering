76171d5a7cb4c981a1ab86bf0a265f5852537983
UL2663 <115477884+UL2663@users.noreply.github.com>
2026-01-07 17:31:44 +0000

    Local backend server prep

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+// backend server JS file 
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3500;
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '..','/public')));
+
+app.get(/\/$|\/mainpage(\.html)?/, (req,res) => {  // regex to handle variations of mainpage 
+    res.sendFile(path.join(__dirname, '..', 'Mainpage.html'));
+});
+
+app.use((req,res) => { 
+    res.status(404).sendFile(path.join(__dirname, '..', '404.html'))});
+
+app.listen(port, () => console.log('server running on port', port));
