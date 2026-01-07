0a44879725395a1fc6dd8cad630608bf18b08c77
UL2663 <115477884+UL2663@users.noreply.github.com>
2026-01-07 22:03:29 +0000

    Final edits pre-render hosting
    Changed roots for image files and css script.
    Moved mainsscript into static frontend.
    
    Centered my 404 error html text because it was bothering me.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 // backend server JS file 
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 const port = process.env.PORT || 3500;
 
 app.use(express.json());
