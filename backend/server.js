c83b5f4d8a117665c6579ca8e36b00a154713e64
UL2663 <115477884+UL2663@users.noreply.github.com>
2026-01-08 19:38:51 +0000

    Re-writing API endpoints in JS
    Rewritten API endpoints in JS using new date functions

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,54 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3500;
+var msdayhours = 1000*60*60*12;
+var last_day = Date.now() - msdayhours;
+var iso_last_day = new Date(last_day).toISOString();
 
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname, '..','/public')));
 
-app.get(/\/$|\/mainpage(\.html)?/, (req,res) => {  // regex to handle variations of mainpage 
+app.get('/api/AInews', async (req, res) => { 
+    try {
+       const params = new URLSearchParams({
+        q: 'artificial intelligence',
+        lang: 'en',
+        from: iso_last_day,
+        max: '10',
+        token: process.env.NEWS_API_KEY
+       })
+     
+    const response = await fetch(`https://gnews.io/api/v4/search?${params}`);
+
+    const data = await response.json();
+    res.json(data)}
+    
+    catch (err) { console.error(err);
+        res.status(500).json({error: "There is an issue with AI News"})
+    }
+});
+
+app.get('/api/ManPharmaAInews', async (req, res) => { 
+    try {
+       const params = new URLSearchParams({
+        q: "(artificial intelligence) AND Manufacturing OR Pharmaceuticals", // OR has higher precedence so should work, ref docs 
+        lang: 'en',
+        from: iso_last_day,
+        max: '10',
+        token: process.env.NEWS_API_KEY
+       })
+     
+    const response = await fetch(`https://gnews.io/api/v4/search?${params}`);
+
+    const data = await response.json();
+    res.json(data)}
+    
+    catch (err) { console.error(err);
+        res.status(500).json({error: "There is an issue with AI Pharma News"})
+    }
+});
+
+app.get(/\/$|\/Mainpage(\.html)?/, (req,res) => {  // regex to handle variations of mainpage 
     res.sendFile(path.join(__dirname, '..', 'Mainpage.html'));
 });
 
