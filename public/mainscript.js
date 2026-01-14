880d62a9e8d13eae960f9369e44a528e6c72702c
UL2663 <115477884+UL2663@users.noreply.github.com>
2026-01-14 11:35:17 +0000

    Adding in weekly calls for Guardian

--- public/mainscript.js
+++ public/mainscript.js
@@ -1,4 +1,5 @@
-//DAILY CALLS
+//DAILY CALLS 
+//gnews
 function getManAI_GN()
     {console.log("call Man AI");
     fetch("/api/ManAIPharmagnews")
@@ -28,40 +29,7 @@ function getAI_GN()
           console.error("fetch error:", err)
      })
     }
-
-//WEEKLY CALLS
-function getAIweek_GN() 
-    {console.log("call AI");
-    fetch("/api/WeekAIgnews")    
-    .then(res =>{             //adding error checking 
-     console.log("status = ", res.status)
-    return res.json();
-})
-    .then(data => { 
-         console.log(data);
-     })
-     .catch(err => {
-          console.error("fetch error:", err)
-     })
-    }
-
-function getManAIWeek_GN()
-{console.log("call Man AI");
-    fetch("/api/WeekManAIPharmagnews")
-    .then(res =>{             //adding error checking 
-     console.log("status = ", res.status)
-    return res.json();
-})
-    .then(data => { 
-         console.log(data);
-     })
-     .catch(err => {
-          console.error("fetch error:", err)
-     })
-    }
-
-    //GUARDIAN 
-
+//guardian 
 function getAI_Guardian(){
     {console.log("call AI Guardian");
         fetch("/api/AIGuardian")
@@ -81,6 +49,71 @@ function getAI_Guardian(){
 function getManAI_Guardian(){
     {console.log("call Man AI Guardian");
         fetch("/api/ManAIPharmaGuardian")
+    .then(res =>{             //adding error checking 
+     console.log("status = ", res.status)
+    return res.json();
+})
+    .then(data => { 
+         console.log(data.response.results);
+     })
+     .catch(err => {
+          console.error("fetch error:", err)
+     })
+    }
+}
+//WEEKLY CALLS
+//gnews
+function getAIweek_GN() 
+    {console.log("call AI");
+    fetch("/api/WeekAIgnews")    
+    .then(res =>{             //adding error checking 
+     console.log("status = ", res.status)
+    return res.json();
+})
+    .then(data => { 
+         console.log(data);
+     })
+     .catch(err => {
+          console.error("fetch error:", err)
+     })
+    }
+
+function getManAIWeek_GN()
+{console.log("call Man AI week");
+    fetch("/api/WeekManAIPharmagnews")
+    .then(res =>{             //adding error checking 
+     console.log("status = ", res.status)
+    return res.json();
+})
+    .then(data => { 
+         console.log(data);
+     })
+     .catch(err => {
+          console.error("fetch error:", err)
+     })
+    }
+
+//GUARDIAN 
+
+function getAI_GuardianWeek(){
+    {console.log("call AI Guardian week");
+        fetch("/api/AIGuardianWeek")
+    .then(res =>{             //adding error checking 
+     console.log("status = ", res.status)
+    return res.json();
+})
+    .then(data => { 
+         console.log(data.response.results);
+     })
+     .catch(err => {
+          console.error("fetch error:", err)
+     })
+    }
+}
+
+function getManAI_GuardianWeek(){
+    {console.log("call Man AI Guardian");
+        fetch("/api/ManAIPharmaGuardianWeek")
     .then(res =>{             //adding error checking 
      console.log("status = ", res.status)
     return res.json();
