f7eaf867b87c597350f174ff7a479309991bdee9
UL2663 <115477884+UL2663@users.noreply.github.com>
2026-01-19 17:16:51 +0000

    Pareto analysis test written

---
+++ backend/tests/paretoValues.test.js
@@ -0,0 +1,16 @@
+const { paretoValues } = require("../utils/analyse")
+
+test("pareto values are accurately built", () => { 
+    const counts = { 
+    ai: 50, //same categories from other test
+    trump: 30,
+    manufacturing: 20};
+
+    const result = paretoValues(counts)
+
+    expect(result).toEqual([
+        {kw: "ai", count: 50, cumulative: 50},
+        {kw: "trump", count: 30, cumulative: 30},
+        {kw: "manufacturing", count: 20, cumulative: 20},
+    ]);
+})
