bdd7676a4f9f7f2582970a4996919c783f0b9c85
UL2663 <115477884+UL2663@users.noreply.github.com>
2026-01-19 20:57:33 +0000

    Radar function in analysis written and minor change to token test

--- backend/utils/analyse.js
+++ backend/utils/analyse.js
@@ -1,5 +1,17 @@
 const extrawords = new Set(["the","and","or","in","as","is","are","from","by","at","with","on","for","to","its", "a"])
 
+const AI_categories = { //aware that these may need to be added to over time  
+  politics: ["trump", "government", "bill", "policy", "election", "labor", "law", "regulation"],
+
+  education: ["education", "learning", "skills", "training", "university", "programme", "students"],
+
+  industry: ["manufacturing", "industry", "economy", "business", "jobs", "employment", "market"],
+
+  health: ["pharma", "pharmaceutical", "medical", "medicine", "health", "drug", "clinical"],
+
+  morals_ethics: ["ethics", "social", "society", "faith", "religion", "speech", "rights", "fairness"]
+}
+
 function stripTokens(text){
     return text.filter(text=> !extrawords.has(text)) //manages edge cases of really short titles 
 }
@@ -32,4 +44,13 @@ function paretoValues(data){
     });
 }
 
-module.exports = { stripTokens, tokenizeTitles, countTokens, paretoValues}
+function radar_funct(data){
+    const out = {};
+    for (const [category, kws] of Object.entries(AI_categories)) {
+        out[category] = kws.reduce(
+            (sum, kw) => sum + (data[kw]||0), 0);
+    }
+    return out
+}
+
+module.exports = { stripTokens, tokenizeTitles, countTokens, paretoValues, radar_funct}
