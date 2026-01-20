95df310dbbb207d7cc3d520a3cbd9f8c9ffec462
UL2663 <115477884+UL2663@users.noreply.github.com>
2026-01-20 18:44:28 +0000

    write functions for formatting output data in to chart.js json blocks

---
+++ backend/utils/format_data.js
@@ -0,0 +1,32 @@
+function formatPareto(values){
+    return{
+        labels: values.map(p => p.kw),
+        
+        datasets: [{
+            type:"bar",
+            label:"Frequency",
+            data: values.map(p => p.count),
+            yAxisID:"y"
+        },
+        {
+            type: "line",
+            label: "cumulative %",
+            data: values.map(p => p.cumulative),
+            yAxisID: "y1"
+        }]
+    }
+}
+
+function formatRadar(values){
+    return{
+        labels: Object.keys(values),
+        datasets : [ 
+            {label: "Distribution of Themes",
+                data: Object.values(values)
+            }]
+    };
+}
+
+function getCount(values){
+    return { amount_of_entries: Object.keys(values).length }
+}
