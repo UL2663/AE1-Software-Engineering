da4b6bc6fedd3864a1fda70012c17c258b9eac2a
UL2663 <115477884+UL2663@users.noreply.github.com>
2026-01-21 21:37:16 +0000

    Bult and tested mapdates and fixed syntax error

--- backend/tests/mapDates.test.js
+++ backend/tests/mapDates.test.js
@@ -1,6 +1,6 @@
 const {mapDates} = require("../utils/analyse")
 
-test("dates are mapped correctly"), () =>{
+test("dates are mapped correctly", () =>{
     const input = [
         {title: "Article on some things", publishedAt: "2026-01-12" , tokens: ["token1","token2"]},
         {title: "Article on some things", publishedAt: "2026-01-12" , tokens: ["token1","token2"]},
@@ -9,4 +9,4 @@ test("dates are mapped correctly"), () =>{
     ]
     const result = mapDates(input)
     expect(result).toEqual({"2026-01-12":2,"2026-01-15":1,"2026-01-25":1} )
-}
+})
